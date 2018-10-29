861b212015f849b7a8f8fe10da0d5432508589da
Richard Clark <34280321+rclark-provar@users.noreply.github.com>
2018-10-29 18:33:59 +0000

    Update MyPageObject.java

--- webinar/src/pageobjects/MyPageObject.java
+++ webinar/src/pageobjects/MyPageObject.java
@@ -7,11 +7,31 @@ import org.openqa.selenium.support.FindBy;
 
 import com.provar.core.testapi.annotations.*;
 
-@Page( title="My Page Object"                                
+@Page( title="MyPageObject"                                
      , summary=""
      , relativeUrl=""
-     , connection="admin"
+     , connection="GoogleSearch"
      )             
 public class MyPageObject {
-			
-}
+
+	@TextType()
+	@FindBy(xpath = "//input[@name=\"q\"]")
+	public WebElement Search;
+	@TextType()
+	@FindByLabel(label = "Search")
+	public WebElement search;
+	@LinkType()
+	@FindBy(linkText = "Salesforce.com For Dummies, 6th Edition (For Dummies (Computer/Tech))")
+	public WebElement salesforceComForDummies6ThEditionForDummiesComputerTech;
+	@ButtonType()
+	@FindByLabel(label = "Add to Cart")
+	public WebElement addToCart;
+	@LinkType()
+	@FindBy(linkText = "Proceed to checkout (1 item)")
+	public WebElement proceedToCheckout1Item;
+	@ButtonType()
+	@FindBy(id = "continue")
+	public WebElement ContinueBtn;
+	@TextType()
+	@FindBy(xpath = "//span[@class='price-large']/../../..//span[@id='newBuyBoxPrice']")
+	public WebElement buyNewOnClicking;
